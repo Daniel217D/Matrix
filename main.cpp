9155decb23596d66c409f88d14feb062d9918084
Daniil <daniel217032001@gmail.com>
2020-03-11 09:59:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,26 +8,27 @@ using namespace Mtrx;
 
 
 int main() {
-    Matrix matrix{};
+    Matrix matrix{}, inverse{};
+
     matrix = create("./inputs/input_0.txt");
     print(&matrix);
-    print(inverse_matrix(matrix), &matrix);
+    print(inverse_matrix(matrix, inverse), &inverse);
     clear(&matrix);
-
+    cout << "----------------------\n";
     matrix = create("./inputs/input_1.txt");
     print(&matrix);
-    print(inverse_matrix(matrix), &matrix);
+    print(inverse_matrix(matrix, inverse), &inverse);
     clear(&matrix);
-
+    cout << "----------------------\n";
     matrix = create("./inputs/input_2.txt");
     print(&matrix);
-    print(inverse_matrix(matrix), &matrix);
+    print(inverse_matrix(matrix, inverse), &inverse);
     clear(&matrix);
-
+    cout << "----------------------\n";
     matrix = create("./inputs/input_hard.txt");
     print(&matrix);
-    print(inverse_matrix(matrix), &matrix);
+    print(inverse_matrix(matrix, inverse), &inverse);
     clear(&matrix);
-
+    cout << "----------------------\n";
     return 0;
 }
