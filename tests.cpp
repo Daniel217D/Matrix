5a90a44abe1015519cb7359dffbf39dff71ab6c9
Daniil <daniel217032001@gmail.com>
2020-05-03 17:56:52 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -11,49 +11,49 @@ using namespace Mtrx;
 const double EPS = 1e-7;
 
 TEST_CASE("Matrix::create", "[Matrix]") {
-    Matrix matrix{};
+    Matrix *matrix = new Matrix;
 
     SECTION("input_0.txt") {
-        matrix.create("./inputs/input_0.txt");
-        REQUIRE(matrix.cols == 3);
-        REQUIRE(matrix.rows == 3);
+        matrix->read("./inputs/input_0.txt");
+        REQUIRE(matrix->getCols() == 3);
+        REQUIRE(matrix->getRows() == 3);
     }
-    matrix.clear();
+    matrix->clear();
 
     SECTION("input_1.txt") {
-        matrix.create("./inputs/input_1.txt");
-        REQUIRE(matrix.cols == 3);
-        REQUIRE(matrix.rows == 2);
+        matrix->read("./inputs/input_1.txt");
+        REQUIRE(matrix->getCols() == 3);
+        REQUIRE(matrix->getRows() == 2);
     }
-    matrix.clear();
+    matrix->clear();
 
     SECTION("input_2.txt") {
-        matrix.create("./inputs/input_2.txt");
-        REQUIRE(matrix.cols == 3);
-        REQUIRE(matrix.rows == 3);
+        matrix->read("./inputs/input_2.txt");
+        REQUIRE(matrix->getCols() == 3);
+        REQUIRE(matrix->getRows() == 3);
     }
-    matrix.clear();
+    matrix->clear();
 
     SECTION("input_3.txt") {
-        matrix.create("./inputs/input_3.txt");
-        REQUIRE(matrix.cols == 3);
-        REQUIRE(matrix.rows == 3);
+        matrix->read("./inputs/input_3.txt");
+        REQUIRE(matrix->getCols() == 3);
+        REQUIRE(matrix->getRows() == 3);
     }
-    matrix.clear();
+    matrix->clear();
 
     SECTION("input_hard.txt") {
-        matrix.create("./inputs/input_hard.txt");
-        REQUIRE(matrix.cols == 5);
-        REQUIRE(matrix.rows == 5);
+        matrix->read("./inputs/input_hard.txt");
+        REQUIRE(matrix->getCols() == 5);
+        REQUIRE(matrix->getRows() == 5);
     }
-    matrix.clear();
+    matrix->clear();
 }
 
 TEST_CASE("Matrix::inverse", "[Matrix]") {
     SECTION("input_0.txt") {
-        Matrix matrix{}, inverse{};
-        matrix.create("./inputs/input_0.txt");
-        int status = matrix.inverse(inverse);
+        Matrix *matrix = new Matrix, *inverse = new Matrix;
+        matrix->read("./inputs/input_0.txt");
+        int status = matrix->inverse(inverse);
 
         REQUIRE(status == 1);
 
@@ -62,12 +62,12 @@ TEST_CASE("Matrix::inverse", "[Matrix]") {
                                     {-1, 0,       1}};
         bool noError = true;
 
-        REQUIRE(inverse.cols == 3);
-        REQUIRE(inverse.rows == 3);
+        REQUIRE(inverse->getCols() == 3);
+        REQUIRE(inverse->getRows() == 3);
 
-        for (int i = 0; i < inverse.rows && noError; ++i) {
-            for (int j = 0; j < inverse.cols && noError; ++j) {
-                if (std::abs(inverse.matrix[i][j] - expectation[i][j]) > EPS) {
+        for (int i = 0; i < inverse->getRows() && noError; ++i) {
+            for (int j = 0; j < inverse->getCols() && noError; ++j) {
+                if (std::abs(inverse->getEl(i, j) - expectation[i][j]) > EPS) {
                     noError = false;
                 }
             }
@@ -77,33 +77,33 @@ TEST_CASE("Matrix::inverse", "[Matrix]") {
     }
 
     SECTION("input_1.txt") {
-        Matrix matrix{}, inverse{};
-        matrix.create("./inputs/input_1.txt");
-        int status = matrix.inverse(inverse);
+        Matrix *matrix = new Matrix, *inverse = new Matrix;
+        matrix->read("./inputs/input_1.txt");
+        int status = matrix->inverse(inverse);
 
         REQUIRE(status == -1);
     }
 
     SECTION("input_2.txt") {
-        Matrix matrix{}, inverse{};
-        matrix.create("./inputs/input_2.txt");
-        int status = matrix.inverse(inverse);
+        Matrix *matrix = new Matrix, *inverse = new Matrix;
+        matrix->read("./inputs/input_2.txt");
+        int status = matrix->inverse(inverse);
 
         REQUIRE(status == 0);
     }
 
     SECTION("input_3.txt") {
-        Matrix matrix{}, inverse{};
-        matrix.create("./inputs/input_3.txt");
-        int status = matrix.inverse(inverse);
+        Matrix *matrix = new Matrix, *inverse = new Matrix;
+        matrix->read("./inputs/input_3.txt");
+        int status = matrix->inverse(inverse);
 
         REQUIRE(status == 0);
     }
 
     SECTION("input_hard.txt") {
-        Matrix matrix{}, inverse{};
-        matrix.create("./inputs/input_hard.txt");
-        int status = matrix.inverse(inverse);
+        Matrix *matrix = new Matrix, *inverse = new Matrix;
+        matrix->read("./inputs/input_hard.txt");
+        int status = matrix->inverse(inverse);
 
         REQUIRE(status == 1);
 
@@ -114,12 +114,12 @@ TEST_CASE("Matrix::inverse", "[Matrix]") {
                                     {2557937./43374293, 3055832./43374293, (-2838064.)/43374293, (-7264839.)/43374293, 260201./43374293}};
         bool noError = true;
 
-        REQUIRE(inverse.cols == 5);
-        REQUIRE(inverse.rows == 5);
+        REQUIRE(inverse->getCols() == 5);
+        REQUIRE(inverse->getRows() == 5);
 
-        for (int i = 0; i < inverse.rows && noError; ++i) {
-            for (int j = 0; j < inverse.cols && noError; ++j) {
-                if (std::abs(inverse.matrix[i][j] - expectation[i][j]) > EPS) {
+        for (int i = 0; i < inverse->getRows() && noError; ++i) {
+            for (int j = 0; j < inverse->getCols() && noError; ++j) {
+                if (std::abs(inverse->getEl(i, j) - expectation[i][j]) > EPS) {
                     noError = false;
                 }
             }
